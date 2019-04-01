f66f69726d6464dfa850b9b3e51b01a4c4aad2ee
ghwlchlaks <ghwlchlaks@naver.com>
2019-04-01 17:37:52 +0900

    샘플 리스트, 카테고리를 이용하여 리스트 출력

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <List v-for="item in items" :key="item.no" :item="item" :categories="categories"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import List from "./components/List.vue";
+import listItem from "./datas/listItem.json";
+import categoryItem from "./datas/categoryItem.json";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    List
+  },
+  data() {
+    return {
+      items: listItem.list,
+      categories: categoryItem.list
+    };
   }
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="list-group">
+    <a class="list-group-item list-group-item-action flex-column align-items-start">
+      <div class="d-flex w-100 justify-content-between"  v-for="category in categories" :key="category.no">
+          <span class="mb-1" v-if="category.no === item.category_no">{{category.name}}</span>
+          <small v-if="category.no === item.category_no">{{item.no}}</small>
+      </div>
+        
+      <div>
+        <span>{{item.email}}</span> |
+        <span>{{item.updated_at}}</span>
+      </div>
+
+      <p class="mb-1">{{item.title}}</p>
+
+      <div>
+        <small>{{item.contents}}</small>
+      </div>
+      
+    </a>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    item: Object,
+    categories: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+</style>
