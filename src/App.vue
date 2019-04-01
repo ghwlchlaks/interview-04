ad6fb1d86e6e083ebb9b9ad50ac9e2936edd6a7b
ghwlchlaks <ghwlchlaks@naver.com>
2019-04-01 18:46:05 +0900

    리스트 구현 및 정렬 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div>
+      <button type="button" class="btn btn-info">필터</button>
+
+      <div class="btn-group btn-group-toggle btn-sort" data-toggle="buttons">
+        <label class="btn btn-secondary active" v-on:click="sortClickHandler(false)">
+          <input type="radio" id="option1" autocomplete="off" checked> 오름차순
+        </label>
+        <label class="btn btn-secondary" v-on:click="sortClickHandler(true)">
+          <input type="radio" id="option2" autocomplete="off"> 내림차순
+        </label>
+      </div>
+    </div>
+
     <List v-for="item in items" :key="item.no" :item="item" :categories="categories"/>
   </div>
 </template>
@@ -20,9 +33,27 @@ export default {
       items: listItem.list,
       categories: categoryItem.list
     };
+  },
+  methods: {
+    sortClickHandler: function(isDesc) {
+      if (isDesc) {
+        // 내림차순
+        this.items.sort(function(a, b) {
+          return +a.no > +b.no ? -1 : +a.no < +b.no ? 1 : 0;
+        });
+      } else {
+        // 올림차순
+        this.items.sort(function(a, b) {
+          return +a.no < +b.no ? -1 : +a.no > +b.no ? 1 : 0;
+        });
+      }
+    }
   }
 };
 </script>
 
 <style lang='less'>
+.btn-sort {
+  float: right;
+}
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="list-group">
     <a class="list-group-item list-group-item-action flex-column align-items-start">
-      <div class="d-flex w-100 justify-content-between"  v-for="category in categories" :key="category.no">
-          <span class="mb-1" v-if="category.no === item.category_no">{{category.name}}</span>
-          <small v-if="category.no === item.category_no">{{item.no}}</small>
+      <div
+        class="d-flex w-100 justify-content-between"
+        v-for="category in categories"
+        :key="category.no"
+      >
+        <span class="mb-1" v-if="category.no === item.category_no">{{category.name}}</span>
+        <small v-if="category.no === item.category_no">{{item.no}}</small>
       </div>
-        
+
       <div>
         <span>{{item.email}}</span> |
         <span>{{item.updated_at}}</span>
@@ -16,9 +20,7 @@
       <div>
         <small>{{item.contents}}</small>
       </div>
-      
     </a>
-
   </div>
 </template>
 
