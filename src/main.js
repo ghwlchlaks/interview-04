4107cd7c96f8a38d7f849abfd645eb1286d5398f
ghwlchlaks <ghwlchlaks@naver.com>
2019-04-01 16:20:53 +0900

    초기 환경 설정: bootstrap, axios, less

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
