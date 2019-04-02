1717857b360b35530d99b43953bbc07ef7ea5f25
ghwlchlaks <ghwlchlaks@naver.com>
2019-04-02 21:23:09 +0900

    vue router 설정 적용 중

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
-import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
+import VueRouter from "vue-router";
+import router from "./router";
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 
 new Vue({
-  render: h => h(App)
+  router
 }).$mount("#app");
---
+++ src/views/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="list-group-item"></div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: {
+    item: Object
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
